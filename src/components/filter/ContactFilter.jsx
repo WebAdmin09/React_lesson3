afd1d79d2f410eb2bb44fabd08c7a631ef90005b
WebAdmin09 <husanorifjonov9@gmail.com>
2023-09-07 11:42:43 +0500

    cssds

--- src/components/filter/ContactFilter.jsx
+++ src/components/filter/ContactFilter.jsx
@@ -1,11 +1,30 @@
 import React, { Component } from 'react'
+import { Form, InputGroup } from "react-bootstrap";
+import { categories } from '../data/categories';
+import style from './ContactFilter.module.css';
 
 export class ContactFilter extends Component {
     render() {
+        const { searching, search, catigorysort, category } = this.props;
         return (
-            <div>
-                
-            </div>
+            <InputGroup className="my-3">
+                <Form.Control
+                    className={style['search']}
+                    value={search}
+                    onChange={searching}
+                    placeholder="Search person......."
+                />
+                <InputGroup.Text className={style['inputText']}>
+                    <Form.Select value={category} onChange={catigorysort}>
+                        <option value="all">All</option>
+                        {categories.map((category) => (
+                            <option key={category} value={category}>
+                                {category}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </InputGroup.Text>
+            </InputGroup>
         )
     }
 }
--- src/pages/formPage.jsx
+++ src/pages/formPage.jsx
@@ -4,7 +4,7 @@ import { Fragment } from 'react';
 import ContactForm from '../components/form/ContactForm';
 import Navbar from '../components/navbar/navbar';
 import ContactFilter from '../components/filter/ContactFilter';
-import { Form, FormControl, Tabs, Tab } from 'react-bootstrap';
+import { Form, Tabs, Tab } from 'react-bootstrap';
 import ContactItem from '../components/contactitem/ContactItem';
 
 export class FormPage extends Component {
@@ -43,33 +43,35 @@ export class FormPage extends Component {
     if (category !== "all") {
       persons = person.filter((person) => person.category === category);
     }
+    const searching = (e) => {
+      this.setState({ search: e.target.value.trim().toLowerCase() })
+    }
     const doneFavourite = persons.filter((person) => person.done);
     const undoneFavourite = persons.filter((person) => !person.done);
     return (
       <Fragment>
         <Navbar />
         <ContactForm />
-        <ContactFilter />
+        <ContactFilter
+          // categorysort={categorysort}
+          search={search}
+          searching={searching} />
         <Form inline>
-          <FormControl
-            type="text"
-            placeholder="Search"
-            className={style['search']}
-          />
+
         </Form>
         <div className={style['tabs__wrapper']}>
-        <Tabs defaultActiveKey="all" variant="pills" className='my-3' justify>
-          <Tab eventKey="all" title="All">
-            {persons.map((person, i) => (
-              <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
-            ))}
-          </Tab>
-          <Tab eventKey="favourite" title="favourite">
-            {undoneFavourite.map((person, i) => (
-              <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
-            ))}
-          </Tab>
-        </Tabs>
+          <Tabs defaultActiveKey="all" variant="pills" className='my-3' justify>
+            <Tab className={style['tab']} eventKey="all" title="All">
+              {persons.map((person, i) => (
+                <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
+              ))}
+            </Tab>
+            <Tab className={style['tab']} eventKey="favourite" title="favourite">
+              {undoneFavourite.map((person, i) => (
+                <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
+              ))}
+            </Tab>
+          </Tabs>
         </div>
       </Fragment>
     )
