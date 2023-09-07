3155dce96073ab4ae5d4d7eb3dec088b785834aa
WebAdmin09 <husanorifjonov9@gmail.com>
2023-09-07 11:16:16 +0500

    dvwsdv

--- src/pages/formPage.jsx
+++ src/pages/formPage.jsx
@@ -1,15 +1,50 @@
-import React, { Component,} from 'react';
+import React, { Component, } from 'react';
 import style from './formPage.module.css'
 import { Fragment } from 'react';
 import ContactForm from '../components/form/ContactForm';
 import Navbar from '../components/navbar/navbar';
 import ContactFilter from '../components/filter/ContactFilter';
-import { Form, FormControl, } from 'react-bootstrap';
-// import ContactItem from '../components/contactitem/ContactItem';
+import { Form, FormControl, Tabs, Tab } from 'react-bootstrap';
+import ContactItem from '../components/contactitem/ContactItem';
 
 export class FormPage extends Component {
+  state = {
+    persons: [
+      {
+        id: 0,
+        lastName: 'John',
+        firstName: 'Brown',
+        phone: '4161616130',
+        category: "family",
+        favourite: false,
+      },
+      {
+        id: 2,
+        lastName: 'Jackie',
+        firstName: 'Chan',
+        phone: '545550',
+        category: "friend",
+        favourite: false,
+      },
+    ],
+    person: {
+      name: "",
+      date: "",
+      category: "family",
+    },
+    search: "",
+    category: "all",
+  };
   render() {
-  
+    let { persons, search, category, person } = this.state;
+    persons = persons.filter((person) => person.lastName.toLowerCase().includes(search));
+
+
+    if (category !== "all") {
+      persons = person.filter((person) => person.category === category);
+    }
+    const doneFavourite = persons.filter((person) => person.done);
+    const undoneFavourite = persons.filter((person) => !person.done);
     return (
       <Fragment>
         <Navbar />
@@ -22,23 +57,20 @@ export class FormPage extends Component {
             className={style['search']}
           />
         </Form>
-        {/* <Tabs defaultActiveKey="all" variant="pills" className="my-3" justify>
-          <Tab eventKey="all" title="All todos">
-            {todos.map((todo, i) => (
-              <ContactItem doneTodo={doneTodo} key={i} no={i + 1} {...todo} />
+        <div className={style['tabs__wrapper']}>
+        <Tabs defaultActiveKey="all" variant="pills" className='my-3' justify>
+          <Tab eventKey="all" title="All">
+            {persons.map((person, i) => (
+              <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
             ))}
           </Tab>
-          <Tab eventKey="undone" title="Undone todos">
-            {undoneTodos.map((todo, i) => (
-              <ContactItem doneTodo={doneTodo} key={i} no={i + 1} {...todo} />
+          <Tab eventKey="favourite" title="favourite">
+            {undoneFavourite.map((person, i) => (
+              <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
             ))}
           </Tab>
-          <Tab eventKey="done" title="Done todos">
-            {doneTodos.map((todo, i) => (
-              <ContactItem doneTodo={doneTodo} key={i} no={i + 1} {...todo} />
-            ))}
-          </Tab>
-        </Tabs> */}
+        </Tabs>
+        </div>
       </Fragment>
     )
   }
