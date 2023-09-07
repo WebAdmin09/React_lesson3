3e00bb98fa76d6313e20560d3deea4250309aeb9
WebAdmin09 <husanorifjonov9@gmail.com>
2023-09-07 14:56:13 +0500

    csds

--- src/components/form/ContactForm.jsx
+++ src/components/form/ContactForm.jsx
@@ -1,41 +1,41 @@
-import React, { Component } from 'react'
-import style from "./ContactForm.module.css"
-import { categories } from '../data/categories'
+import React, { Component } from 'react';
+import style from "./ContactForm.module.css";
+import { categories } from '../data/categories';
 
 export class ContactForm extends Component {
   render() {
     return (
       <div className='container'>
-        <div className={style['form']}>
+        <form onSubmit={this.props.submit} className={style['form']}>
           <div className={style['fullname']}>
-            <label htmlFor="name" className={style['input__label']}>
+            <label htmlFor="firstName" className={style['input__label']}>
               <span className={style['name__span']}>FirstName</span>
-              <input className={style['input__name']} type="name"required />
+              <input id="firstName" name="firstName" className={style['input__name']} type="text" required />
             </label>
-            <label htmlFor="name" className={style['input__label']}>
+            <label htmlFor="lastName" className={style['input__label']}>
               <span className={style['name__span']}>LastName</span>
-              <input className={style['input__name']} type="name" required />
+              <input id="lastName" name="lastName" className={style['input__name']} type="text" required />
             </label>
           </div>
           <div className={style['selectphone']}>
             <label htmlFor="phone" className={style['input__label']}>
               <span className={style['name__span']}>Phone</span>
-              <input className={style['input__name']} type="number" required />
+              <input id="phone" name="phone" className={style['input__name']} type="number" required />
             </label>
-            <label htmlFor="name" className={style['input__labelsection']}>
-              <span className={style['name__span']}>Contact catigory</span>
-              <select className={style['select__btn']} required>
+            <label htmlFor="category" className={style['input__labelsection']}>
+              <span className={style['name__span']}>Contact Category</span>
+              <select id="category" name="category" className={style['select__btn']} required defaultValue="family">
                 {categories.map((el) => (
                   <option className={style['option']} key={el} value={el}>{el}</option>
                 ))}
               </select>
             </label>
           </div>
-          <button className={style['add__btn']}>Add person</button>
-        </div>
+          <button type="submit" className={style['add__btn']}>Add person</button>
+        </form>
       </div>
-    )
+    );
   }
 }
 
-export default ContactForm
+export default ContactForm;
--- src/pages/formPage.jsx
+++ src/pages/formPage.jsx
@@ -1,6 +1,5 @@
-import React, { Component, } from 'react';
-import style from './formPage.module.css'
-import { Fragment } from 'react';
+import React, { Component } from 'react';
+import style from './formPage.module.css';
 import ContactForm from '../components/form/ContactForm';
 import Navbar from '../components/navbar/navbar';
 import ContactFilter from '../components/filter/ContactFilter';
@@ -28,54 +27,78 @@ export class FormPage extends Component {
       },
     ],
     person: {
-      name: "",
+      lastName: 'John',
       date: "",
       category: "family",
     },
     search: "",
     category: "all",
   };
+
   render() {
     let { persons, search, category, person } = this.state;
-    persons = persons.filter((person) => person.lastName.toLowerCase().includes(search));
 
+    persons = persons.filter((p) =>
+      p.lastName.toLowerCase().includes(search.toLowerCase())
+    );
 
     if (category !== "all") {
-      persons = person.filter((person) => person.category === category);
+      persons = persons.filter((p) => p.category === category);
     }
-    const searching = (e) => {
-      this.setState({ search: e.target.value.trim().toLowerCase() })
-    }
-    const doneFavourite = persons.filter((person) => person.done);
-    const undoneFavourite = persons.filter((person) => !person.done);
-    return (
-      <Fragment>
-        <Navbar />
-        <ContactForm />
-        <ContactFilter
-          // categorysort={categorysort}
-          search={search}
-          searching={searching} />
-        <Form inline>
 
-        </Form>
+    const searching = (e) => {
+      this.setState({ search: e.target.value.trim().toLowerCase() });
+    };
+
+    const categorysort = (e) => {
+      this.setState({ category: e.target.value });
+    };
+
+    const submit = (e) => {
+      e.preventDefault();
+      let newpersons = [...persons, { ...person, done: false, id: Date.now() }];
+      this.setState({
+        persons: newpersons,
+        person: { lastName: 'John', category: "family" },
+      });
+    };
+
+    const handleperson = (e) => {
+      let newperson = { ...person, [e.target.id]: e.target.value };
+      this.setState({ person: newperson });
+    };
+
+    // const doneFavourite = persons.filter((p) => p.favourite);
+    const undoneFavourite = persons.filter((p) => !p.favourite);
+
+    return (
+      <div>
+        <Navbar />
+        <ContactForm person={person} submit={submit} handleperson={handleperson} />
+        <ContactFilter
+          categorysort={categorysort}
+          category={category}
+          search={search}
+          searching={searching}
+        />
+        <Form inline></Form>
         <div className={style['tabs__wrapper']}>
           <Tabs defaultActiveKey="all" variant="pills" className='my-3' justify>
             <Tab className={style['tab']} eventKey="all" title="All">
               {persons.map((person, i) => (
-                <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
+                <ContactItem key={i} no={i + 1} {...person} />
               ))}
             </Tab>
-            <Tab className={style['tab']} eventKey="favourite" title="favourite">
+            <Tab className={style['tab']} eventKey="favourite" title="Favourite">
               {undoneFavourite.map((person, i) => (
-                <ContactItem doneTodo={doneFavourite} key={i} no={i + 1} {...person} />
+                <ContactItem key={i} no={i + 1} {...person} />
               ))}
             </Tab>
           </Tabs>
         </div>
-      </Fragment>
-    )
+      </div>
+    );
   }
 }
 
-export default FormPage
+export default FormPage;
